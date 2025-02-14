e51b910ca60f8ccb3ba1487cf3fd215c2f853eb3
Jennifer Le <122183026+JenniferMLe@users.noreply.github.com>
2025-02-13 20:47:43 -0600

    added choropleth map

--- Study on the Health of Older Adults/app.R
+++ Study on the Health of Older Adults/app.R
@@ -8,6 +8,14 @@ library(shiny)
 library(shinythemes)
 library(ggplot2)
 
+questions_sorted <- sort(c(unique(as.character(df$Question))))
+
+get_qID <- function(input){
+  question_id <- questions_fct_tbl %>% 
+    filter(Question.y == input) %>% pull(QuestionID)
+  return(question_id)
+}
+
 dashboard1 <-
   tabPanel(
     "Explore Dataset",
@@ -30,8 +38,9 @@ dashboard2 <-
     "Ranking by Question",
     sidebarLayout(
       sidebarPanel(
-        selectInput('Question2','Question:', c(unique(as.character(df$Question)))),
-        selectInput('GroupBy','Group By:', c('Location', 'Age_Group', 'Gender_or_Race'))
+        selectInput('Question2','Question:', questions_sorted,
+                    selected='Self-reported health of "fair" or "poor"'),
+        selectInput('GroupBy','Group By:', c('Gender_or_Race','Location','Age_Group'))
       ),
       mainPanel(
         plotOutput('barplot')
@@ -44,18 +53,31 @@ dashboard3 <-
     "Correlation Exploration",
     sidebarLayout(
       sidebarPanel(
-        selectInput("x_axis", "Question 1 (x-axis):", c(unique(as.character(df$Question))),
+        selectInput("x_axis", "Question 1 (x-axis):", questions_sorted,
                     selected="Experiencing frequent mental distress"),
-        selectInput("y_axis", "Question 2 (y-axis):", c(unique(as.character(df$Question))),
+        selectInput("y_axis", "Question 2 (y-axis):", questions_sorted,
                     selected="Smoking every day or some days"),
         verbatimTextOutput("cor"),
         "Interestingly, having a physical or mental disability has a much weaker
         correlation with frequent mental distress than smoking."
       ),
       mainPanel(
-        plotOutput('scatterplot')
+        plotlyOutput('scatterplot')
       )
     )
+  )
+
+dashboard4 <-
+  tabPanel(
+    "Map",
+    fluidRow(
+      column(4, selectInput("QuestionMap", "Question", questions_sorted, 
+                            selected="Have a lifetime diagnosis of depression")),
+      column(4, selectInput("Color", "Color Theme", c("Blue", "Purple", "Green", "Orange"))),
+      column(1, checkboxInput("allYears", "All Years", value = TRUE)),
+      column(3, sliderInput("Year", "Year", 2015, 2022, 2015, step=1, sep=""))
+    ),
+    plotlyOutput('map')
   )
 
 # Define UI
@@ -65,7 +87,8 @@ ui <- fluidPage(
     "Analysis of Older Adult's Health",
     dashboard1,
     dashboard2,
-    dashboard3
+    dashboard3,
+    dashboard4
   ) 
 ) 
 
@@ -95,14 +118,13 @@ server <- function(input, output, session) {
   
   output$barplot <- renderPlot({
     # get question_id from question user chooses
-    question_id <- questions_fct_tbl %>% 
-      filter(Question.y == input$Question2) %>% pull(QuestionID)
+    qID <- get_qID(input$Question2)
     
     # create a data frame summarizing results, use get() to transform string id to variable
     df_res <- df_pivot %>% 
-      filter(!is.na(get(question_id))) %>% 
+      filter(!is.na(get(qID))) %>% 
       group_by(get(input$GroupBy)) %>% 
-      summarize(avg_percent = mean(get(question_id))) %>% 
+      summarize(avg_percent = mean(get(qID))) %>% 
       arrange(avg_percent) 
     
     # Get the first column name safely
@@ -137,46 +159,76 @@ server <- function(input, output, session) {
     
   }, height = 600)
   
-  output$scatterplot <- renderPlot({
+  output$scatterplot <- renderPlotly({
+    # get question id from question input
+    qID1 <- get_qID(input$x_axis)
+    qID2 <- get_qID(input$y_axis)
     
-    # get question_id from question1 
-    question_id1 <- questions_fct_tbl %>% 
-      filter(Question.y == input$x_axis) %>% pull(QuestionID)
-    
-    # get question_id from question2
-    question_id2 <- questions_fct_tbl %>% 
-      filter(Question.y == input$y_axis) %>% pull(QuestionID)
-    
+    # create result set that filters out null values
     df_res <- df_pivot %>% 
-      filter(!is.na(get(question_id1)) & !is.na(get(question_id2)))
+      filter(!is.na(get(qID1)) & !is.na(get(qID2)))
     
-    ggplot(df_res, mapping=aes(x=get(question_id1), y=get(question_id2))) + 
-      geom_point() + geom_smooth() +
-      labs(
-        x=input$x_axis,
-        y=input$y_axis
-      ) +
-      theme(
-        axis.title = element_text(size = 16),
-        axis.text = element_text(size = 13)
-      ) 
-  }, height=600)
+    # create scatter plot
+    plot_ly(
+      data = df_res,
+      x = ~get(qID1),
+      y = ~get(qID2),
+      type = "scatter",
+      mode = "markers",
+      marker = list(size = 7, color = 'black'),
+      text = ~paste(
+        "x: ", get(qID1), "<br>",
+        "y: ", get(qID2), "<br>",
+        Location, "<br>",
+        Gender_or_Race, "<br>",
+        Age_Group, "<br>", sep=""
+      ),
+      hoverinfo = "text"
+    ) %>%
+      layout(
+        xaxis = list(title = input$x_axis),
+        yaxis = list(title = input$y_axis),
+        height = 600
+      )
+    
+    # ggplot graph code below
+    # plot <- ggplot(df_res, mapping=aes(
+    #     x=get(qID1), 
+    #     y=get(qID2),
+    #     text(paste(
+    #       "x: ", x, "<br>",
+    #       "y: ", y, "<br>",
+    #       Location, "<br>",
+    #       Gender_or_Race, "<br>",
+    #       Age_Group, "<br>", sep=""
+    #     ))
+    #   )) +
+    #   geom_point() + geom_smooth() +
+    #   labs(
+    #     x=input$x_axis,
+    #     y=input$y_axis
+    #   ) +
+    #   theme(
+    #     axis.title = element_text(size = 16),
+    #     axis.text = element_text(size = 13)
+    #   )
+    # 
+    # ggplotly(plot, tooltip = "text")
+    
+  })
   
   output$cor <- renderText({
-    # get question_id from question1 
-    question_id1 <- questions_fct_tbl %>% 
-      filter(Question.y == input$x_axis) %>% pull(QuestionID)
-    
-    # get question_id from question2
-    question_id2 <- questions_fct_tbl %>% 
-      filter(Question.y == input$y_axis) %>% pull(QuestionID)
+    # get question id from question input
+    qID1 <- get_qID(input$x_axis)
+    qID2 <- get_qID(input$y_axis)
     
     # use = "complete.obs" removes rows with null values before computing the value
-    ce <- cor(df_pivot[[question_id1]], df_pivot[[question_id2]], use = "complete.obs")
+    ce <- cor(df_pivot[[qID1]], df_pivot[[qID2]], use = "complete.obs")
     ce <- round(ce, digits = 3)
     
     strength <- ""
     
+    # compute correlation strength
     if(0 <= abs(ce) & abs(ce) <= 0.1) {
       strength <- "No or very weak correlation"
     } else if(0.1 < abs(ce) & abs(ce) <= 0.3) {
@@ -189,12 +241,64 @@ server <- function(input, output, session) {
       strength <- "Very Strong correlation"
     }
     
-    paste("Correlation Coefficient: ", ce, " \n\n-> ", strength, 
-          "\n\n",
-          sep="")
+    paste("Correlation Coefficient: ", ce, " \n\n-> ", strength, "\n\n", sep="")
+  })
+  
+  output$map <- renderPlotly({
+    # get question ID from choosen question
+    qID <- get_qID(input$QuestionMap)
+    
+    df_res <- NULL
+    
+    # if user doesn't want to filter by year
+    if(input$allYears){
+      df_res <- df_pivot %>% 
+        filter(!is.na(get(qID))) %>% 
+        group_by(Location) %>% 
+        summarize(avg_percent = mean(get(qID))) %>% 
+        arrange(avg_percent) 
+    } 
+    # if user wants to filter by year
+    else {
+      df_res <- df_pivot %>% 
+        filter(!is.na(get(qID)) & YearStart == input$Year & YearEnd == input$Year) %>% 
+        group_by(Location) %>% 
+        summarize(avg_percent = mean(get(qID))) %>% 
+        arrange(avg_percent) 
+    }
+    
+    # create themes that will change depending on user input
+    theme <- c()
+    if(input$Color == "Blue") {
+      theme <- c("#e8f0ff", "#a5c2ff","#1862fb", "#001a50")
+    }else if (input$Color == "Purple") {
+      theme <- c("#f8efff", "#dfb8ff","#9411ff", "#1f0039")
+    } else if (input$Color == "Green") {
+      theme <- c("#e4f5e9", "#91daa6","#008324", "#00230a")
+    } else if (input$Color == "Orange") {
+      theme <- c("#ffebd1", "#ffb95a","#d37b00", "#461e00")
+    }
+    
+    # merge the result set with location dataframe so that we can use a map
+    df_res <- merge(df_res, location, by="Location")
+    
+    # create plotly interactive map
+    fig <- plot_ly(
+      data = df_res,
+      locations = ~LocationAbbr,  
+      locationmode = "USA-states",
+      z = ~avg_percent,  
+      type = "choropleth",
+      colorscale = list(
+        c(0, 0.333, 0.666, 1), theme
+      )
+    ) %>%
+      layout(geo = list(scope = "usa"))  
+    
+    fig  
   })
 } # server
 
 
 # Create Shiny object
-shinyApp(ui = ui, server = server)
+shinyApp(ui,server)
--- Study on the Health of Older Adults/data_cleaning.R
+++ Study on the Health of Older Adults/data_cleaning.R
@@ -1,10 +1,14 @@
-df<- read.csv("Healthy_Aging_Data.csv")
+df<- read.csv("../../Healthy_Aging_Data.csv")
 
 # remove rows with no data
 df <- filter(df, !is.na(Data_Value) | !is.na(Data_Value_Alt))
 
 # create table mapping symbol to footnotes
 footnote <- distinct(df, Data_Value_Footnote_Symbol, Data_Value_Footnote)
+
+# create mapping from location to longitude and latitude
+location <- distinct(df, LocationAbbr, LocationDesc, Geolocation)
+location <- rename(location, Location = LocationDesc)
 
 # create table mapping question ID to question
 questions_fct_tbl <- df %>% 
@@ -22,10 +26,6 @@ questions_shortened <- read_csv("questions_shortened.csv")
 df <- merge(x=df, y=questions_shortened, by="QuestionID")
 questions_fct_tbl <- merge(x=questions_fct_tbl, y= questions_shortened, by='QuestionID')
 rm(questions_shortened)
-
-# replace values
-df$Stratification1[df$Stratification1 == 'Overall'] <- '50+'
-df$Stratification2[df$Stratification2 == ''] <- 'Overall'
 
 # remove unnecessary columns and rename columns
 df <- df %>% 
@@ -51,35 +51,20 @@ df <- df %>%
     Location = LocationDesc
   ) 
 
+# replace values
+df$Age_Group[df$Age_Group == 'Overall'] <- '50+'
+df$Gender_or_Race[df$Gender_or_Race == ''] <- 'Overall'
+df$Gender_or_Race[df$Gender_or_Race == 'Black, non-Hispanic'] <-'Black'
+df$Gender_or_Race[df$Gender_or_Race == 'White, non-Hispanic'] <-'White'
+
 # pivot table so each question gets its own column
 df_pivot = select(df, 'YearStart','YearEnd','Location',
                   'Age_Group', 'Gender_or_Race','QuestionID','Value')
+
 df_pivot = pivot_wider(df_pivot, names_from = QuestionID, values_from = Value)
 
-df_res <- df_pivot %>% 
-  filter(!is.na(Q01)) %>% 
-  group_by(Gender_or_Race) %>% 
-  summarize(avg_percent = mean(Q01)) %>% 
-  arrange(avg_percent) 
-
-plot <- 
-  ggplot(data = df_res, 
-    mapping = aes(y=reorder(Gender_or_Race, avg_percent), x=avg_percent)) + 
-  geom_col(fill='skyblue') +
-  labs(y="Gender or Race") +
-  geom_text(aes(label = sprintf("%.1f",avg_percent))) +
-  theme(
-    axis.text.x = element_text(size = 10),  # X-axis tick label size
-    axis.text.y = element_text(size = 10)   # Y-axis tick label size
-  )
-
-plot
-
-ggplot(df_pivot, mapping=aes(x=Q01, y=Q02)) + geom_point()
-
-# ggplotly(plot, tooltip = 'x')
-
-result <- distinct(df, Topic)
+# create plots to test here
+result <- distinct(df, Gender_or_Race)
 result <- arrange(result, YearStart)
 
 
